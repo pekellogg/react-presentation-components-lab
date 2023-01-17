801ef727ae198a54aba10682fc41615a9278a32d
Peyton Kellogg <pekellogg@hotmail.com>
2023-01-16 18:41:36 -0800

    commit tree 2612993e9071f2d68d357f5dfa2feb3e652b3157

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,27 @@
-// Code SimpleComponent Here
+import React, { Component } from "react";
+
+class SimpleComponent extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      mood: "happy"
+    };
+  };
+
+  clickHandler = () => {
+    const newMood = this.state.mood === "happy" ? "sad" : "happy";
+    this.setState({ mood: newMood });
+  };
+
+  render() {
+    return (
+      <div onClick={this.clickHandler}>
+        {this.state.mood}
+      </div>
+    );
+  };
+
+};
+
+export default SimpleComponent;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => alert("clicked!")} />
   </div>,
   document.getElementById('root')
 );
